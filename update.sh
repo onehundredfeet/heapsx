1f3cab6263d283e713c369c929a8e10ab9593f24
onehundredfeet <rcleven@mac.com>
2021-11-13 18:20:45 -0800

    Add HL Steam

--- update.sh
+++ update.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
 git submodule update --remote --recursive --merge
+
+if [ -z "$1" ] 
+then
+    echo "No project directory supplied"
+else
+echo "Using direcotry $1"
+
+pushd $1 &> /dev/null
+haxelib upgrade hscript
+haxelib upgrade tink_core
+haxelib upgrade tink_macro
+haxelib upgrade hlsdl
+haxelib upgrade haxe-concurrent
+popd &> /dev/null
+
+echo "Completed setting up $1"
+fi
